ff6af279ff845fd29f0648bfca5676fd99dd8ed5
eu-larissasouza <larissa.alves.souza@outlook.com>
2025-11-04 20:47:09 -0300

    projeto: APPiadas

---
+++ appiadas/README.md
@@ -0,0 +1,2 @@
+# appiadas-kotlin
+Pasta destinada ao app "APPiadas" desenvolvido como revisão para o Checkpoint 1 na matéria de Android Kotlin Developer do curso de Sistemas de Informação na FIAP (3° ano).
---
+++ appiadas/app/src/main/java/com/fiap/appiadas/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.fiap.appiadas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.fiap.appiadas.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        binding.btNewJoker.setOnClickListener() {
+            showJoker();
+        }
+
+    }
+
+    private fun showJoker() {
+        val jokers = resources.getStringArray(R.array.jokers)
+        var numJoker = Random.nextInt(jokers.size)
+        val joker = jokers[numJoker]
+        binding.tvJoker.text = joker;
+    }
+}
